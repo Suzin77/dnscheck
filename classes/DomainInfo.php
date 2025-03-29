47920cb8cbba882c29510b43be08c5b01da5d1cd
Patrycjusz Sosnowski <patrycjusz.sosnowski@gmail.com>
2025-03-29 08:13:38 +0100

    add whois domain registrar

--- classes/DomainInfo.php
+++ classes/DomainInfo.php
@@ -3,9 +3,13 @@
 class DomainInfo {
     private $domain;
     private $registrar = '';
+    private $registrarUrl = '';
+    private $registrarPhone = '';
+    private $registrarEmail = '';
     private $registrant = '';
     private $creationDate = '';
     private $expirationDate = '';
+    private $lastModified = '';
     private $status = '';
 
     public function __construct(string $domain) {
@@ -29,6 +33,21 @@ class DomainInfo {
                 $this->registrar = trim($matches[1]);
             }
             
+            // URL rejestratora
+            if (preg_match('/https?:\/\/[^\s]+/m', $whois, $matches)) {
+                $this->registrarUrl = trim($matches[0]);
+            }
+            
+            // Telefon rejestratora
+            if (preg_match('/Tel:\s*(.+)$/m', $whois, $matches)) {
+                $this->registrarPhone = trim($matches[1]);
+            }
+            
+            // Email rejestratora
+            if (preg_match('/Email:\s*(.+)$/m', $whois, $matches)) {
+                $this->registrarEmail = trim($matches[1]);
+            }
+            
             // Próba wyciągnięcia informacji o właścicielu
             if (preg_match('/registrant type:\s*(.+)$/m', $whois, $matches)) {
                 $this->registrant = trim($matches[1]);
@@ -42,6 +61,11 @@ class DomainInfo {
             // Data wygaśnięcia domeny
             if (preg_match('/renewal date:\s*(.+)$/m', $whois, $matches)) {
                 $this->expirationDate = trim($matches[1]);
+            }
+
+            // Data ostatniej modyfikacji
+            if (preg_match('/last modified:\s*(.+)$/m', $whois, $matches)) {
+                $this->lastModified = trim($matches[1]);
             }
 
             // Status domeny
@@ -58,10 +82,18 @@ class DomainInfo {
         }
 
         return [
-            'registrar' => $this->registrar,
+            'registrar' => [
+                'name' => $this->registrar,
+                'url' => $this->registrarUrl,
+                'phone' => $this->registrarPhone,
+                'email' => $this->registrarEmail
+            ],
             'registrant' => $this->registrant,
-            'creation_date' => $this->creationDate,
-            'expiration_date' => $this->expirationDate,
+            'dates' => [
+                'creation' => $this->creationDate,
+                'expiration' => $this->expirationDate,
+                'last_modified' => $this->lastModified
+            ],
             'status' => $this->status
         ];
     }
