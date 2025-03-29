513a82664cd70e7c6affa38cd4fa7c288e31e2fe
Patrycjusz Sosnowski <patrycjusz.sosnowski@gmail.com>
2025-03-29 17:35:44 +0100

    logs

--- check_dns.php
+++ check_dns.php
@@ -9,9 +9,17 @@ require_once 'classes/TXTRecord.php';
 require_once 'classes/CNAMERecord.php';
 require_once 'classes/DomainInfo.php';
 require_once 'classes/DNSChecker.php';
+require_once 'log_search.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $domain = $_POST['domain'] ?? '';
+    $domain = $_POST['domain'] ?? 'unknown';
+    $ipAddress = $_SERVER['REMOTE_ADDR'];
+    $referrer = $_SERVER['HTTP_REFERER'] ?? 'unknown';
+    $date = date('Y-m-d H:i:s');
+
+    $logEntry = "Data: $date, IP: $ipAddress, Referrer: $referrer, Domena: $domain\n";
+    file_put_contents('logs/log.txt', $logEntry, FILE_APPEND);
+
     $checker = new DNSChecker($domain);
     echo json_encode($checker->checkAll());
 } else {
